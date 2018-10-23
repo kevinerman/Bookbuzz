8abecfc49d72461716d3dcf3f52a661e77c1412e
Kevin Erman <kevinerman@gmail.com>
2018-10-22 19:11:09 -0500

    updated file structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+
+
+const App = () => (
+
+<div>
+<SearchResultContainer />
+</div>
+);
+
+export default App; 
