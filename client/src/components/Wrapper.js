d2d0c1f75c3d05cfe87bde5107905b76aa79fec7
Rion <rionirvin>
2018-10-23 11:54:51 -0500

    Book Lay out styling

---
+++ client/src/components/Wrapper.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import { Link} from "react-router-dom";
+import './Wrapper.css';
+
+const Wrapper = (props) => (
+  <div>
+   <div id="wrapper">
+        <div id="container">
+    
+            <section className="open-book">
+                <header>
+                    <h1>BookBuzz</h1>
+                    <h6>Project 3</h6>
+                </header>
+                <article>
+                    <h2 className="chapter-title">BookBuzz</h2>
+                    
+                    {props.children}
+                </article>
+                <footer>
+                    <ol id="page-numbers">
+                        <li>1</li>
+                        <li>2</li>
+                    </ol>
+                </footer>
+            </section>
+        </div>
+    </div>
+
+
+  </div>
+);
+
+export default Wrapper;
