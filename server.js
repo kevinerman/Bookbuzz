22710c3995f9827d02a5a011d9766082843d606c
Kevin Erman <kevinerman@gmail.com>
2018-10-27 13:16:30 -0500

    heroku live

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
